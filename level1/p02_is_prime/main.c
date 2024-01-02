10505730acbb045203f5d66e3ed1434125caa025
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-02 15:28:06 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,12 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
-    return 0;
+#include<stdio.h>
+int main()
+{ int num;
+    scanf("%u",&num);
+    for(int i=2;i<num;i++){
+        if(i==num-1){printf("%u is a prime.",num);
+            break;}
+        else if(num%i!=0)continue;
+        else {printf("%u is not a prime.",num);
+            break;}
+    }return 0;
 }
