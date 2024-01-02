25489f4dc8d967b009dad7738a2cd73cfc87aca3
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-02 15:27:17 +0800

    Update main.c

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
+#include <stdio.h>
+#include<windows.h>
+int main(){while(1)
+    {int i=1;
+        for (;i < 121;i += 1)
+        {
+            printf("A"),
+                    Sleep(100),
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+                    printf("\b ");
+
+        }
+        for(;i>=121&&i<242;i+=1)
+        {printf("\b\b  ");
+            printf("\bA \b\b");
+            Sleep(100);
+
+        }
+        for (; i ==242;) { i = 1; }
+    }
 }
+
