66048d72627382f86eb3c31e3903cb3bb8316561
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-02 15:31:47 +0800

    Update main.c

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,26 @@
-#include <printf.h>
+#include <stdio.h>
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+void move(char A,char C)
+{
+	printf("%c->%c\n",A,C);
+}
+
+void hanoi(int n,char A,char B,char C)
+{
+	if (n==1) {	
+    	move(A,C);
+	}else {   
+	    hanoi(n-1,A,C,B);
+	    move(A,C);
+	    hanoi(n-1,B,A,C);
+	}
+}
+int main( ) {	
+	int n;
+	
+	printf("input number:");
+	scanf("%d",&n);
+	printf("the steps to move %2d diskes:\n",n);
+	hanoi(n,'a','b','c');   
+	return 0;
 }
