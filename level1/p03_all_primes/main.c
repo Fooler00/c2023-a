524c88f051f8a1522f6182ef2f94ed82c4ed46f1
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-02 15:28:48 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,16 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include<stdio.h>
+#include<time.h>
+int main()
+{clock_t start,end;
+    start=clock();
+    printf("2 ");
+    for(int i=3;i>=2&&i<=1000;i++) {
+        for (int j = 2; j < i; j++) {
+            if (j == i - 1)printf("%d ", i);
+            else if (i % j != 0)continue;
+            else break;
+        }
+    }end=clock();
+    printf("\ntime=%f",((double)(end-start))/CLK_TCK);
     return 0;
 }
