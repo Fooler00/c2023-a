bebedc7da8a9a8bc933e1b7fcd9fb4c738efbc08
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-02 15:34:38 +0800

    Update main.c

--- level1/p08_push_boxes/main.c
+++ level1/p08_push_boxes/main.c
@@ -12,19 +12,16 @@ int map[9][11]={
         {1,0,4,4,0,4,0,0,0,0,1},
         {1,0,4,4,0,4,4,3,0,1,1},
         {1,1,1,1,1,1,1,1,1,1,0}
-};//原始的图表，五行六列，其中 0 代表着空白的地方； 1 代表着墙；2 代表着人；
-//3 代表着箱子；4 代表着箱子的终点位置。
-
-//图的变化要靠自己来编写数组，通过数字来进行图的构造。
+};
 int drawmain();
 int tuidong();
 int winshu();
 
-int main()//主函数
+int main()
 {
     while(1)
     {
-        system("cls");//对其进行清屏
+        system("cls");
         drawmain();
         tuidong();
 
@@ -32,12 +29,12 @@ int main()//主函数
     printf("shuchu \n");
     return 0;
 }
-//把图形刻画出来
+
 
 int drawmain()
 {
     int i,j;
-    winshu();//调用输赢的函数
+    winshu();
     for(i=0;i<9;i++)
     {
         for(j=0;j<11;j++)
@@ -45,7 +42,7 @@ int drawmain()
             switch(map[i][j])
             {
                 case 0:
-                    printf("  "); //空白的地方
+                    printf("  "); 
                     break;
                 case 1:
                     printf("■"); //墙
@@ -57,13 +54,13 @@ int drawmain()
                     printf("☆"); //箱子
                     break;
                 case 4:
-                    printf("◎"); //终点地方
+                    printf("◎"); //终点
                     break;
                 case 6:
-                    printf("♂");//人加终点位置
+                    printf("♂");//人加终点
                     break;
                 case 7:
-                    printf("★") ;//箱子加终点位置
+                    printf("★") ;//箱子加终点
                     break;
             }
         }
@@ -71,7 +68,6 @@ int drawmain()
     }
 }
 
-//进行小人的移动，整个移动的过程就是数组变化的过程
 int tuidong()
 {
     int count,caw=0;//行和列
@@ -87,17 +83,11 @@ int tuidong()
         }
     }
 
-    tui=getch();//与getchar()有区别的是：getchar()输入一个字符后需要回车来进行下一个字符的输入，
-    //比较麻烦 ，getch()则不需要回车就能连续输入多个字符。
+    tui=getch();
     switch(tui)
-    {//上
+    {
         case 'W':
         case 72:
-            // 1.人的前面是空地；
-            // 2.人的前面是终点位置；
-            // 3.人的前面是箱子
-            //3.1.箱子的前面是空地；
-            //3.2.箱子的前面是终点位置。
             if(map[count-1][caw]==0||map[count-1][caw]==4)
             {
                 map[count][caw]-=2;
@@ -113,10 +103,8 @@ int tuidong()
                 }
             }
             break;
-
-//下
         case 'S':
-        case 80://键值
+        case 80:
             if(map[count+1][caw]==0||map[count+1][caw]==4)
             {
                 map[count][caw]-=2;
@@ -133,7 +121,6 @@ int tuidong()
                 }
             }
             break;
-//左
         case 'A':
         case 75:
             if(map[count][caw-1]==0||map[count][caw-1]==4)
@@ -152,7 +139,6 @@ int tuidong()
                 }
             }
             break;
-//右
         case 'D':
         case 77:
             if(map[count][caw+1]==0||map[count][caw+1]==4)
@@ -177,7 +163,7 @@ int tuidong()
 
 int winshu()
 {
-    int k = 0;//初始化
+    int k = 0;
     int j,i;
     for(i=0;i<9;i++)
     {
