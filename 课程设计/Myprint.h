7845a002a0dae161d31a7f88af98d32f234e3e76
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-04 19:08:31 +0800

    Update Myprint.h

--- 课程设计/Myprint.h
+++ 课程设计/Myprint.h
@@ -53,43 +53,7 @@ public:
     COORD currentPos = {-3, -3};
     COORD waitPos;
     std::string atk[4]={""};
-    int chessboard[16][16] = {0};
-    int flag = -1;
-    const int step = 30;
-
-    void printboard();
-
-    void printchess();
-
-    void printmouse();
-
-    void printact();
-
-    bool chessjudge(int x,int y,int chessboard[16][16]);
-
-    void printend();
-
-    void printstart();
-
-    void Attack(int x,int y,int flag,int Vmboard[16][16],int ScoreBoard[16][16]);
-
-    int AllScore(Node_Tree *p);
-
-    Node_Tree* createroot();
-
-    Node_Tree* createleaf(Node_Tree *p);
-
-    Node_Tree* Minimax(Node_Tree* p);
-
-    void PrintScore(Node_Tree *p);
-
-    Node_Tree* createlist(Node_Tree *p);
-
-    void Robot();
-
-    int ScoreGet(int empty,int count,int block,int roable);
-
-};
+   
 
 
 #endif //MYPRINT_H
