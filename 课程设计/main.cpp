4d85cb56eb26cffe6c38e83078d5efa82194e919
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-04 21:35:51 +0800

    Create main.cpp

---
+++ 课程设计/main.cpp
@@ -0,0 +1,30 @@
+#include <graphics.h>
+#include <conio.h>
+#include "Myprint.h"
+using namespace std;
+int main()
+{
+    Myprint chess;
+    initgraph(480, 480);
+    chess.printboard();
+    chess.printstart();
+    /*while(true){
+        chess.printboard();
+        chess.printchess();
+        if (chess.chessjudge(chess.currentPos.Y,chess.currentPos.X,chess.chessboard)){
+            chess.printend();
+            break;
+        }
+        if (chess.flag==-1) {
+            chess.printact();
+            chess.printmouse();
+        }
+        else{
+            chess.Robot();
+            chess.printact();
+        }
+    }可取消注释*/
+    getch();
+    closegraph();
+    return 0;
+}
