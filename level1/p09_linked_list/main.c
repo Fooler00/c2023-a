5fcf121cf86fc24b0637210204ae3ae621349a2d
Fooler00 <144091962+Fooler00@users.noreply.github.com>
2024-01-05 13:35:35 +0800

    Update main.c

--- level1/p09_linked_list/main.c
+++ level1/p09_linked_list/main.c
@@ -4,15 +4,16 @@
 
 typedef struct ListNode ListNode;
 struct ListNode {
-      int value;
-      struct ListNode *next;
-  };
-struct ListNode *creat(int n){
+    int value;
+    struct ListNode *next;
+};
+
+struct ListNode *creat(int n) {
     struct ListNode *head, *node, *end;
-    head = (ListNode*)malloc(sizeof(ListNode));
+    head = (ListNode *) malloc(sizeof(ListNode));
     end = head;
     for (int i = 0; i < n; i++) {
-        node = (ListNode *)malloc(sizeof(ListNode ));
+        node = (ListNode *) malloc(sizeof(ListNode));
         scanf("%d", &node->value);
         end->next = node;
         end = node;
@@ -20,31 +21,31 @@ struct ListNode *creat(int n){
     end->next = NULL;
     return head;
 }
-struct ListNode* reverseList(struct ListNode* head) {
-    struct ListNode* prev = NULL;
-    struct ListNode* curr = head;
+
+struct ListNode *reverseList(struct ListNode *head) {
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
     while (curr) {
-        struct ListNode* next = (*curr).next;
+        struct ListNode *next = (*curr).next;
         (*curr).next = prev;
         prev = curr;
         curr = next;
     }
     return prev;
 }
-ListNode *findNode(ListNode * head, int value) {
-    ListNode * current = head;
-    struct ListNode *i;
-    i=(ListNode*)malloc(sizeof(ListNode ));
+
+ListNode *findNode(ListNode *head, int value) {
+    ListNode *current = head;
     while (current != NULL) {
-        if (current->value == value) { return i; }
-        else current->next=current;
+        if (current->value == value) { return current; }
+        else current->next = current;
     }
     return NULL;
 }
 
 int find(ListNode *head, int value, int j) {
     ListNode *current = head;
-    for (int i = 0; i < j; i++) {
+    for (int t = 0; t < j; t++) {
         current->next = current;
     }
     while (1) {
@@ -54,19 +55,18 @@ int find(ListNode *head, int value, int j) {
     return 0;
 }
 
-int main()
-{
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     struct ListNode *i;
-    i=(ListNode*)malloc(sizeof(ListNode ));
-    i->next=creat(n);
+    i = (ListNode *) malloc(sizeof(ListNode));
+    i->next = creat(n);
     i->next = reverseList(i->next);
     if (findNode(i->next, 5) == NULL)printf("-1");
     else {
-        for (int j = 0; j < n; j++) {
-            if (find(i->next, 5, j) != 0)printf("%d ", find(i->next, 5, j));
-            else printf("");
+    for (int j = 0; j < n; j++) {
+        if (find(i->next, 5, j) != 0)printf("%d ", find(i->next, 5, j));
+        else printf("");
         }
     }
     return 0;
